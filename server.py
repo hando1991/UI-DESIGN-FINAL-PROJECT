8cb05776d0f3c256c9ff6b11ddda44ab5efd479f
bliu17 <93516882+bliu17@users.noreply.github.com>
2022-04-24 17:24:40 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -8,29 +8,34 @@ data = {
     "1":{
         "id":"1",
         "name": "Receivables",
-        "property":["Receivables changes once the sales of a business has made.","The amount of money received for goods or services.","The amount of money owed at the end of each month varies (debtors)."],
+        "property":["The sales a business has made.","The amount of money received for goods or services.","The amount of money owed at the end of each month varies (debtors)."],
         "image":"https://cdn.wallstreetmojo.com/wp-content/uploads/2019/07/Accounts-Receivable-Examples.jpg",
+        "video":"https://www.youtube.com/embed/uuJDmY6mA6Q autoplay=1&mute=1 ",
         "summary":"Accounts receivable, abbreviated as AR or A/R,are legally enforceable claims for payment held by a business for goods supplied or services rendered that customers have ordered but not paid for. These are generally in the form of invoices raised by a business and delivered to the customer for payment within an agreed time frame. Accounts receivable is shown in a balance sheet as an asset. It is one of a series of accounting transactions dealing with the billing of a customer for goods and services that the customer has ordered. These may be distinguished from notes receivable, which are debts created through formal legal instruments called promissory notes."
 
     },
     "2":{
         "id":"2",
         "name": "Inventory",
-        "property":["Inventory is the raw materials used to produce goods as well as the goods that are available for sale.","It is classified as a current asset on a company's balance sheet.","The three types of inventory include raw materials, work-in-progress, and finished goods. ","Inventory is valued in one of three ways, including the first-in, first-out method; the last-in, first-out method; and the weighted average method."],
+        "property":["Inventory is the raw materials used to produce goods as well as the goods that are available for sale.","The three types of inventory include raw materials, work-in-progress, and finished goods. ","Inventory is valued in one of three ways, including the first-in, first-out method; the last-in, first-out method; and the weighted average method."],
         "image":"https://assets-global.website-files.com/600fe6e1ff56087409a9f096/602b9f28bb288a2b7eea1217_Inventory-Reduction-Strategies-For-Excess-Or-Slow-Moving-Stock.jpg",
-        "summary":"The term inventory refers to the raw materials used in production as well as the goods produced that are available for sale. A company's inventory represents one of the most important assets it has because the turnover of inventory represents one of the primary sources of revenue generation and subsequent earnings for the company's shareholders. There are three types of inventory, including raw materials, work-in-progress, and finished goods. It is categorized as a current asset on a company's balance sheet."
+        "video":"https://www.youtube.com/embed/trreap294A0",
+        "summary":"The term inventory refers to the raw materials used in production as well as the goods produced that are available for sale. A company's inventory represents one of the most important assets it has because the turnover of inventory represents one of the primary sources of revenue generation and subsequent earnings for the company's shareholders. There are three types of inventory, including raw materials, work-in-progress, and finished goods.It is categorized as a current asset on a company's balance sheet."
     },
     "3":{
         "id":"3",
         "name": "Property Plant and Equipment",
         "property":["Property, plant, and equipment (PP&E) are long-term assets vital to business operations and the long-term financial health of a company.","Equipment, machinery, buildings, and vehicles are all types of PP&E assets."," (PP&E) are also called fixed or tangible assets, meaning they are physical items that a company cannot easily liquidate."],
         "image":"https://accountingplay.com/wp-content/uploads/2015/08/H_10F_Property_plant_and-equipment.png",
+        "video":"https://www.youtube.com/embed/T-HZgYPwEKA",
         "summary":"Property, plant, and equipment (PP&E) are long-term assets vital to business operations. Property, plant, and equipment are tangible assets, meaning they are physical in nature or can be touched; as a result, they are not easily converted into cash. The overall value of a company's PP&E can range from very low to extremely high compared to its total assets."
     },
     "4":{
         "id":"4",
         "name":"",
         "image":"",
+        "video":"",
+
         "summary":""
     },
     "5":{
@@ -173,6 +178,10 @@ def directStart():
 @app.route('/learn1/',methods=['GET', 'POST'])
 def learn1():
     return render_template('entry.html')
+
+@app.route('/explore/',methods=['GET', 'POST'])
+def explore():
+    return render_template('explore.html')
 
 @app.route('/learn2/',methods=['GET', 'POST'])
 def learn2():
